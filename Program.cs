a2bd0335647eab6fc9dc54a555a1cc03e025d0dd
KarolinaJanzSowa <karolinajanz@gmail.com>
2023-06-29 22:07:40 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Zadaniee2_Programowanie2023
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Obliczenia kartka= Obliczenia.ArkuszPapieru("A3");
+            Console.WriteLine($"Bok A (wysokość): {kartka.BokA} mm");
+            Console.WriteLine($"Bok B (szerokość): {kartka.BokB} mm");
+            Console.WriteLine($"Wymiary: {kartka.BokB}x{kartka.BokA} mm");
+
+            Console.ReadLine();
+        }
+    }
+}
